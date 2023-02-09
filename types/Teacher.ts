487a2bcb81e3065ab944bc179830f331e4c9bb5f
Assar <33634310+assarbertil@users.noreply.github.com>
2023-02-09 13:37:40 +0100

    Fixed build error

--- types/Teacher.ts
+++ types/Teacher.ts
@@ -2,7 +2,7 @@ import { Profile } from "./Profile"
 import { Database } from "./supabase"
 
 export type Teacher = Database["public"]["Tables"]["teachers"]["Row"] & {
-  profiles: Pick<Profile, "name" | "picture">
+  profiles: Pick<Profile, "name">
   numberOfCourses: number
   totalNumberOfStudents: number
 }
